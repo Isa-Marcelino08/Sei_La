0a2e82caf1505c7246c9ab025f2f3051f26ca485
Isabely Rodriguês <isabely.kusmitsch.dev2024@gmail.com>
2024-12-02 09:47:20 -0300

    Add files via upload

---
+++ somativa1/PrjIsabelySomativa1/src/main/java/com/example/demo/PrjIsabelySomativa1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjIsabelySomativa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjIsabelySomativa1Application.class, args);
+	}
+
+}
